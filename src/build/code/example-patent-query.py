d4fa07f744bb396f91c1d2c9cd862fc914c865a8
Jidé Anene <dominicanene@gmail.com>
2020-05-07 22:10:16 -0500

    merge

--- src/build/code/example-patent-query.py
+++ src/build/code/example-patent-query.py
@@ -11,10 +11,25 @@ PROJECT_ID = "patents-research-275923"
 
 
 bq_assistant = BigQueryHelper("patents-public-data", "patents")
-
 pdDF = bq_assistant.head("publications", num_rows=3)
-bq_assistant.table_schema("publications")
 
-# citations
-pdDF["citation"]
+# if in the schema its says its a `RECORD` then you have to unset it or do something special cuz records are arrays can
+# return arrays in select, just atomic ele like strings, numbers, etc
+# can check by looking @the output for a dataset w/ 3 rows
+pdDF["cpc.code"]
 pdDF["publication_date"]
+
+
+
+QUERY="""
+SELECT DISTINCT publication_number
+FROM `patents-public-data.patents.publications`,
+unnest(cpc) as cpc
+WHERE substr(cpc.code, 1,4) IN ('A01G', 'A01H') 
+OR substr(cpc.code, 1,3) IN ('C07', 'C08', 'C09') 
+LIMIT 3;
+"""
+
+df = bq_assistant.query_to_pandas(QUERY)
+print(df)
+
